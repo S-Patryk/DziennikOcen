f00dfba88fad23b95d223d20f0f2b1d7b8dabb16
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2024-12-08 17:17:50 +0100

    add GetUser client

---
+++ DziennikOcen.Client/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.Xml;
+using System;
+
+namespace DziennikOcen.Client.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Pin { get; set; }
+        public int RoleID { get; set; }
+        public DateTime Create_at { get; set; }
+        public DateTime Update_at { get; set; }
+    }
+}
