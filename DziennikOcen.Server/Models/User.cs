19b10b54a5b8fbf844f781fbcf665e38cd8c003d
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2024-12-08 18:12:03 +0100

    add users table

--- DziennikOcen.Server/Models/User.cs
+++ DziennikOcen.Server/Models/User.cs
@@ -5,7 +5,6 @@ namespace DziennikOcen.Server.Models
 {
     public class User
     {
-    
         public int UserID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
