863d76b52a8270773bfd9b9ece3dbd275c3428ae
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2024-12-08 17:00:50 +0100

    add fake users

--- DziennikOcen.Server/Controllers/UserController.cs
+++ DziennikOcen.Server/Controllers/UserController.cs
@@ -3,17 +3,39 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DziennikOcen.Server.Controllers
 {
-    
+
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-            
-
         [HttpGet]
         public List<User> Get()
         {
-                return new List<User>() { new Models.User { Create_at=DateTime.Now} };
+            var fakeUsers = new List<User>()
+            {
+                new User()
+                {
+                    Update_at = DateTime.MinValue,
+                    Create_at = DateTime.Now,
+                    Password = "saddsad",
+                    Pin = 123,
+                    RoleID = 1,
+                    UserID = 1,
+                    Username = "jacek"
+                },
+                new User()
+                {
+                    Update_at = DateTime.MinValue,
+                    Create_at = DateTime.Now,
+                    Password = "saddsad",
+                    Pin = 123,
+                    RoleID = 1,
+                    UserID = 2,
+                    Username = "patryk"
+                },
+            };
+
+            return fakeUsers;
         }
     }
 }
